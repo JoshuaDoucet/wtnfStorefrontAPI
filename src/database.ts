874be4f30da9c4b19b55cb83a1eeeb0e023eea8e
Joshua Doucet <joshua.doucet@live.com>
2022-02-06 11:36:51 -0700

    db config update for prod

--- src/database.ts
+++ src/database.ts
@@ -10,10 +10,15 @@ dotenv.config();
 const {
   ENV,
   POSTGRES_HOST,
+  POSTGRES_PROD_HOST,
+  POSTGRES_PROD_PORT,
   POSTGRES_DB,
   POSTGRES_TEST_DB,
+  POSTGRES_PROD_DB,
   POSTGRES_USER,
-  POSTGRES_PASSWORD
+  POSTGRES_PROD_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_PROD_PASSWORD
 } = process.env;
 
 let client: Pool;
@@ -28,13 +33,24 @@ if (ENV === 'test') {
   });
 }
 // create a connection to the developmnent DB
-else {
+else if (ENV === 'dev'){
   client = new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD
   });
+}
+else if (ENV === 'prod'){
+  client = new Pool({
+    host: POSTGRES_PROD_HOST,
+    port: parseInt((POSTGRES_PROD_PORT as unknown) as string),
+    database: POSTGRES_PROD_DB,
+    user: POSTGRES_PROD_USER,
+    password: POSTGRES_PROD_PASSWORD
+  });
+}else{
+  throw new Error('Invalid ENV to init DB')
 }
 
 export default client;
