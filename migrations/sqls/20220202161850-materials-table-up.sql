bafd0c26752222436e74a9fc7d508413fa39d8a4
Joshua Doucet <joshua.doucet@live.com>
2022-02-10 11:29:46 -0700

    fix error with material sample data

--- migrations/sqls/20220202161850-materials-table-up.sql
+++ migrations/sqls/20220202161850-materials-table-up.sql
@@ -3,7 +3,9 @@ CREATE TABLE materials (
      name VARCHAR NOT NULL
 );
 
-INSERT INTO materials (name) VALUES ('Leather');
-INSERT INTO materials (name) VALUES ('Cotton');
-INSERT INTO materials (name) VALUES ('Silk');
-INSERT INTO materials (name) VALUES ('Gold');
+INSERT INTO materials (name) VALUES ('Leather'),
+                                   ('Cotton'),
+                                   ('Silk'),
+                                   ('Gold'),
+                                   ('Animal-Free Leather'),
+                                   ('Wool');
